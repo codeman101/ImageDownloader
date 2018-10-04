b74f72a291b8f2454d7f6e5b0e0150a6dedafc6b
Frank <logicalman101@gmail.com>
2018-10-04 00:58:25 -0700

    comment fixes

--- background.js
+++ background.js
@@ -1,20 +1,30 @@
 //--------------------------------------------------------------------------------------------------
 // Name:
-// btnClick
+// listenForClicks
 // GF
 // Arguments:
 // None
 //
 //
 // About:
-// Calls content.js to be executed.
+// gets image tabs from current webpage and connects them to another tag that has a download attribute so when 
+// the given image gets clicked the user will be showed a prompt to download the image.
+//
+//Name: btnClick
+//
+//About: function is executed when addon icon is clicked and it tries to run listenForClick function for cureent tab
 //
 //
 // Line at the end:
 // adds a listener so that function will only execute when addon icon is clicked
+//
+//
+// Additional note: During the development of this I learned that it's not possible to run a temporary addon in firefox 
+// for any tab other than the one that loaded the temporary addon. Thereofre the main code in the addon that does the 
+// majority of the work was tested in a web console to make sure it worked for pages with images.
 //--------------------------------------------------------------------------------------------------
 
-function listenForClick(tabs)
+function listenForClick()
 {
     var imgs = document.getElementsByTagName('IMG')
     var tag;
